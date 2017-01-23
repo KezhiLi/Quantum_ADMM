0ff1e6aaad0885412e5b923f91e80d241b4509e6
kezhili <kezhi.li@imperial.ac.uk>
2017-01-23 11:07:40 +0000

    Update
    added comments

--- project2Hermitian_singular_shrink_fast.m
+++ project2Hermitian_singular_shrink_fast.m
@@ -1,24 +1,25 @@
-function [ rho ] = project2Hermitian_singular_shrink_fast( rho ,alp,d,svd_sNo)
+function [ rho ] = project2Hermitian_singular_shrink_fast(rho ,alp,d,svd_sNo)
 rho=reshape(rho,d,d);
+% Hermitian matrix
 rho=(rho+rho')/2;
- 
-%[V,D,S]=svd(rho);
- [V,D,S]=rsvd(rho,svd_sNo);
- %diagD=diag((max(abs(D)-tao,0)).*sign(D));
- 
- 
- diagD=diag(D);
- diagDD=max(diagD-alp,0);
- diagD=diagDD+min(diagD+alp,0);
 
- rho=V*diag(diagD)*S';
-  %
- if abs(trace( rho))>0.000001
+% fast svd
+[V,D,S]=rsvd(rho,svd_sNo);
+
+% shrinkage
+diagD=diag(D);
+diagDD=max(diagD-alp,0);
+diagD=diagDD+min(diagD+alp,0);
+
+rho=V*diag(diagD)*S';
+
+% trace of density matrix is 1
+if abs(trace( rho))>1e-6
     rho = rho/((abs(trace(rho))*0.9+1*0.1));
     %rho = rho/(abs(trace(rho)));
- end
- %
- rho=reshape(rho,d*d,1);
- 
+end
+%
+rho=reshape(rho,d*d,1);
+
 end
 
